a0ca453728f72027c64708b699e822597c192bef
buffge <1515888956@qq.com>
2019-01-31 17:18:56 +0800

    nat init

--- 1/main.go
+++ 1/main.go
@@ -2,9 +2,85 @@ package main
 
 import (
 	"fmt"
-	"runtime"
+	"log"
+	"net"
+	"time"
 )
 
-func main() {
-	fmt.Println(runtime.NumCPU())
+var (
+	pl = fmt.Println
+)
+
+func startClient(ip string, port int) {
+	conn, err := net.Dial("tcp", fmt.Sprintf("%s:%d", ip, port))
+	if err != nil {
+		log.Println(err)
+		return
+	}
+	defer conn.Close()
+	if err != nil {
+		pl("服务器端", conn.RemoteAddr(), "已关闭连接...")
+		return
+	}
+	buf := make([]byte, 1024)
+	pl("服务器端地址 ", conn.RemoteAddr())
+	localConn, err := net.Dial("http", fmt.Sprintf("127.0.0.1:80"))
+	if err != nil {
+		log.Println(err)
+		return
+	}
+	localConn.Read(buf)
+
+	for {
+		conn.Read(buf)
+		pl("服务器端的返回是 ", byte2Str(buf))
+	}
+	for _ = range time.Tick(5 * time.Second) {
+		_, err = conn.Write([]byte("ping"))
+		if err != nil {
+			pl("ping失败:", err.Error())
+			return
+		}
+	}
 }
+func byte2Str(p []byte) string {
+	for i := 0; i < len(p); i++ {
+		if p[i] == 0 {
+			return string(p[0:i])
+		}
+	}
+	return string(p)
+}
+func main() {
+	startClient("47.96.13.188", 7788)
+}
+
+/*
+	nat
+	客户端1 client c1
+	客户端2 client c2
+	服务器 server s
+c1 -> s
+		s获取到c1的端口
+3.打洞过程
+
+双打洞客户端
+（1）A请求Server。
+（2）B请求Server。
+（3）Server把A的IP和端口信息发给B。
+（4）Server把B的IP和端口信息发给A。
+（5）A利用信息给B发消息。（A信任B）
+（6）B利用信息给A发消息。（B信任A）
+
+单打洞客户端
+    A 请求 S
+	S 保存 A 的 端口 并且为A 分配一个域名
+	S 定时 发送消息给 A
+	A 定时 发送消息给 S
+	B 用A分配到的域名访问 S http请求
+	S 发送消息给A
+	A 转发消息到本地的80端口
+	A返回消息给S
+	S 返回给B
+
+*/
