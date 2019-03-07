d438b154053b18b87f7c7b0dabda730fb6d7f68f
buffge <1515888956@qq.com>
2019-03-07 23:47:00 +0800

    rpc demo

---
+++ rpc/client/main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"github.com/buffge/gobyexample/rpc"
+	"log"
+	"net"
+	"net/rpc/jsonrpc"
+)
+
+// type Args struct {
+//     A int `json:"A"`
+//     B int `json:"B"`
+// }
+
+func main() {
+	conn, err := net.Dial("tcp", ":7788")
+	if err != nil {
+		panic(err)
+	}
+	client := jsonrpc.NewClient(conn)
+	var sum int
+	err = client.Call("CalcService.Add", rpcdemo.Args{1, 4}, &sum)
+	log.Println(sum, err)
+	err = client.Call("CalcService.Add", rpcdemo.Args{1324, 344}, &sum)
+	log.Println(sum, err)
+	//报错 json: cannot unmarshal number into Go value of type rpcdemo.Args
+	err = client.Call("CalcService.Add", 1, &sum)
+	log.Println(sum, err)
+}
