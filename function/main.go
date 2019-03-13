395df62b695aab1f6aeac26cefa833b1a2c6c298
buffge <1515888956@qq.com>
2019-03-13 23:46:12 +0800

    测试 init函数

--- function/main.go
+++ function/main.go
@@ -2,22 +2,26 @@ package main
 
 import "fmt"
 
+// 引入了这个包首先就会执行它的init函数
+import _ "github.com/buffge/gobyexample/function/sub"
+
 func main() {
 	fmt.Println(sum0(2, 34))
 	fmt.Println(sum1(2, 34))
 	fmt.Println(sum1(2, 34, 0x78, 0x12))
 	fmt.Println(sum1(2, 34, 34, 234, 234, 234))
 	numArr := []int{1, 2, 3, 4}
-	//... 结构数组
+	// ... 结构数组
 	fmt.Println(sum1(numArr...))
-	//匿名函数
+	// 匿名函数
 	total := sum2()
 	fmt.Println(total(1))
 	fmt.Println(total(2))
 	fmt.Println(total(3))
 	fmt.Println(total(4))
 	fmt.Println(min(53, 31, 643, 124, 435, 124, 35, 32, 4, 21))
-
+	// 在test.go 中也引入sub.go 但是因为init执行过了，所以不会再次执行
+	HelloWorld()
 }
 
 /**
@@ -38,7 +42,7 @@ func sum1(numArr ...int) (total int) {
 	return
 }
 
-//匿名函数
+// 匿名函数
 
 func sum2() func(b int) int {
 	a := 0
