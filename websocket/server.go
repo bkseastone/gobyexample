ffa3a7da4f08a6b70c5eefef9f70e8b10ece9330
buffge <1515888956@qq.com>
2019-10-09 14:13:37 +0800

    websocket 初步

---
+++ websocket/server.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"log"
+	"net/http"
+)
+
+func serveHome(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path != "/" {
+		http.Error(w, "Not found", http.StatusNotFound)
+		return
+	}
+	if r.Method != "GET" {
+		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+		return
+	}
+	http.ServeFile(w, r, "html/index.html")
+}
+func main() {
+	hub := newHub()
+	go hub.run()
+	http.HandleFunc("/", serveHome)
+	http.HandleFunc("/ws", func(w http.ResponseWriter, r *http.Request) {
+		serveWs(hub, w, r)
+	})
+	addr := ":9988"
+	err := http.ListenAndServe(addr, nil)
+	if err != nil {
+		log.Fatal("ListenAndServe: ", err)
+	}
+}
