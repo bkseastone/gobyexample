0f8eeb1e4b9d326a3b2cc8363b1816e3bf94fd8b
buffge <1515888956@qq.com>
2020-11-28 17:46:45 +0800

    bufio

--- file/main.go
+++ file/main.go
@@ -1,29 +1,64 @@
 package main
 
 import (
-	"fmt"
+	"bufio"
+	"bytes"
 	"io"
 	"io/ioutil"
+	"log"
 	"os"
 	"path/filepath"
 	"syscall"
+	"time"
 )
 
-var pl = fmt.Println
-
 func main() {
+
+}
+func testBufIo() {
+	var bt1b = bytes.Repeat([]byte{1}, 1)
+	var bt1k = bytes.Repeat([]byte{1}, 1<<10)
+	var bt3k = bytes.Repeat([]byte{1}, 1024*3)
+	n := 100000
+	testWrite(n, &bt1b)
+	testWrite(n, &bt1k)
+	testWrite(n, &bt3k)
+	testBufWrite(n, &bt1b)
+	testBufWrite(n, &bt1k)
+	testBufWrite(n, &bt3k)
+}
+func testWrite(n int, data *[]byte) {
+	f1, _ := ioutil.TempFile("", "testgo")
+	defer f1.Close()
+	t := time.Now()
+	for i := 0; i < n; i++ {
+		_, _ = f1.Write(*data)
+	}
+	log.Println(time.Since(t).Milliseconds())
+}
+func testBufWrite(n int, data *[]byte) {
+	f1, _ := ioutil.TempFile("", "testgo")
+	defer f1.Close()
+	writer := bufio.NewWriterSize(f1, 1<<20)
+	t := time.Now()
+	for i := 0; i < n; i++ {
+		_, _ = writer.Write(*data)
+	}
+	log.Println(time.Since(t).Milliseconds())
+}
+func test1() {
 	isFile, _ := IsFile("./sdfsdf")
-	pl("判断是否是文件", isFile)
+	log.Println("判断是否是文件", isFile)
 	isDir, _ := IsDir("./tmpDir")
-	pl("判断是否是目录", isDir)
+	log.Println("判断是否是目录", isDir)
 	fileSize, _ := FileSize("./tmp")
-	pl("文件的大小是", fileSize)
-	//写入文件
+	log.Println("文件的大小是", fileSize)
+	// 写入文件
 	FilePutContents("./t0", "t0士大夫士大夫123", 0644)
-	//读取文件
+	// 读取文件
 	t0Content, _ := FileGetContents("./t0")
-	pl("to文件中的内容为", t0Content)
-	//删除文件
+	log.Println("to文件中的内容为", t0Content)
+	// 删除文件
 	Unlink("./t0")
 }
 
