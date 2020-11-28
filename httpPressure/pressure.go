68d57d902e6b28247c75e3461cfac3e31d29a68c
buffge <1515888956@qq.com>
2020-11-28 17:46:33 +0800

    添加 超时选项

--- httpPressure/pressure.go
+++ httpPressure/pressure.go
@@ -3,6 +3,8 @@ package main
 import (
 	"crypto/tls"
 	"flag"
+	"io"
+	"io/ioutil"
 	"log"
 	"net/http"
 	"os"
@@ -11,32 +13,31 @@ import (
 	"time"
 )
 
-var trash *os.File
+var client = http.Client{
+	Transport: &http.Transport{
+		DisableCompression: true,
+		TLSClientConfig: &tls.Config{
+			InsecureSkipVerify: true,
+		},
+	},
+}
+var req *http.Request
 
 func main() {
-	var err error
-	if trash, err = os.OpenFile(os.DevNull, os.O_WRONLY, 0200); err != nil {
-		panic(err)
-	}
 	var (
-		url, durationStr, tickerDelayStr string
-		concurrent                       int
+		url, durationStr, tickerDelayStr, timeoutStr string
+		concurrent                                   int
 	)
 	flag.StringVar(&url, "url", "", "要访问的url")
 	flag.IntVar(&concurrent, "c", 100, "并发数")
 	flag.StringVar(&durationStr, "time", "30s", "压测的时间 eg. 30s 1h")
+	flag.StringVar(&timeoutStr, "timeout", "30s", "请求超时时间 eg. 30s 1m")
 	flag.StringVar(&tickerDelayStr, "ticker", "10s", "每隔多久显示当前压测情况 不填则不显示")
 	flag.Parse()
-	// url = "https://test.buffge.com/yarn.lock"
-	// url = "https://nqhjdadn.zrbwlqa.com:10001/v1/users/register"
-	// url = "http://chaojiappguanli.oss-accelerate.aliyuncs.com/apk/f7b632eea5b64b08b09aa6e44b1bfaa1/f7b632eea5b64b08b09aa6e44b1bfaa1.apk"
-	// concurrent = 2000
-	// durationStr = "60m"
-	// tickerDelayStr = "10s"
-	pressure(url, concurrent, durationStr, tickerDelayStr)
-}
-func pressure(url string, concurrency int, durationStr, tickerDelayStr string) {
-	var successCount, failedCount, contentLen int
+	url = "http://localnor.com:8898/prom"
+	concurrent = 10000
+	durationStr = "60m"
+	tickerDelayStr = "10s"
 	duration, err := time.ParseDuration(durationStr)
 	if err != nil {
 		panic(err)
@@ -45,13 +46,23 @@ func pressure(url string, concurrency int, durationStr, tickerDelayStr string) {
 	if err != nil {
 		panic(err)
 	}
+	timeout, err := time.ParseDuration(timeoutStr)
+	if err != nil {
+		panic(err)
+	}
+	client.Timeout = timeout
+	req, _ = http.NewRequest(http.MethodGet, url, nil)
+	pressure(url, concurrent, duration, tickerDelay)
+}
+func pressure(url string, concurrency int, duration, tickerDelay time.Duration) {
+	var successCount, failedCount, contentLen int
 	quit := time.After(duration)
 	begin := time.Now()
 	log.Println("start pressure")
 	contItemLen := make(chan int, concurrency)
-	fail := make(chan byte, concurrency)
+	fail := make(chan struct{}, concurrency)
 	for i := 0; i < concurrency; i++ {
-		go spider(url, contItemLen, fail)
+		go spider(contItemLen, fail)
 	}
 	var outputContentLenTicker *time.Ticker
 	if tickerDelay != 0 {
@@ -62,27 +73,29 @@ func pressure(url string, concurrency int, durationStr, tickerDelayStr string) {
 					continue
 				}
 				duration := t.Sub(begin)
-				log.Printf(" ticker 共用时%s\n 成功%d次\n 失败%d次\n 总字节数%.2fM\n 每秒字节数:%.1fM\n 每秒请求数: %."+
-					"1f\n",
+				lenMb := float64(contentLen / 1024 / 1024)
+				durationSec := duration.Seconds()
+				log.Printf(" ticker 共用时%s\n 成功%d次\n 失败%d次\n 总字节数%.2fM\n"+
+					" 每秒字节数:%.1fM\n 每秒请求数: %.1f\n",
 					duration, successCount, failedCount,
-					float32(contentLen/1024/1024),
-					float64(contentLen/1024/1024)/duration.Seconds(),
-					float64(successCount)/duration.Seconds(),
+					lenMb,
+					lenMb/durationSec,
+					float64(successCount)/durationSec,
 				)
 			}
 		}()
 	}
-	signQuit := make(chan os.Signal, 1)
+	signQuit := make(chan os.Signal)
 	signal.Notify(signQuit, syscall.SIGINT, syscall.SIGTERM)
 	for {
 		select {
 		case t := <-contItemLen:
 			successCount++
 			contentLen += t
-			go spider(url, contItemLen, fail)
+			go spider(contItemLen, fail)
 		case <-fail:
 			failedCount++
-			go spider(url, contItemLen, fail)
+			go spider(contItemLen, fail)
 		case <-quit:
 			goto stop
 		case <-signQuit:
@@ -90,12 +103,14 @@ func pressure(url string, concurrency int, durationStr, tickerDelayStr string) {
 		}
 	}
 stop:
-	end := time.Now()
-	duration = end.Sub(begin)
-	log.Printf("访问%s\n 共用时%v\n 成功%d次\n 失败%d次\n 总字节数%.1fM\n 每秒字节数:%.1fM\n 每秒请求数: %.1f\n",
-		url, duration, successCount, failedCount, float32(contentLen/1024/1024),
-		float64(contentLen/1024/1024)/duration.Seconds(),
-		float64(successCount)/duration.Seconds(),
+	duration = time.Since(begin)
+	lenMb := float64(contentLen / 1024 / 1024)
+	durationSec := duration.Seconds()
+	log.Printf("访问%s\n 共用时%v\n 成功%d次\n 失败%d次\n 总字节数%.1fM\n"+
+		" 每秒字节数:%.1fM\n 每秒请求数: %.1f\n",
+		url, duration, successCount, failedCount, lenMb,
+		lenMb/durationSec,
+		float64(successCount)/durationSec,
 	)
 	log.Println("end pressure")
 	if outputContentLenTicker != nil {
@@ -103,23 +118,13 @@ stop:
 	}
 }
 
-var client = http.Client{
-	Transport: &http.Transport{
-		DisableCompression: true,
-		TLSClientConfig: &tls.Config{
-			InsecureSkipVerify: true,
-		},
-	},
-}
-
-func spider(url string, contItemLen chan int, fail chan byte) {
-	resp, err := client.Get(url)
+func spider(contItemLen chan int, fail chan struct{}) {
+	resp, err := client.Do(req)
 	if err != nil {
-		fail <- 0
+		fail <- struct{}{}
 		return
 	}
 	defer resp.Body.Close()
-	length, _ := trash.ReadFrom(resp.Body)
-	// log.Println(err)
+	length, _ := io.Copy(ioutil.Discard, resp.Body)
 	contItemLen <- int(length)
 }
